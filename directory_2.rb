6a939380219a42fafa823489b1a045a88c6b16f7
SimonTanner <simonthetanner@hotmail.com>
2017-04-02 17:59:04 +0000

    Exercise 10 - Refactoring

--- directory_2.rb
+++ directory_2.rb
@@ -1,20 +1,18 @@
+@students = [] # an empty array accessible to all methods
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
 def print_header
@@ -22,7 +20,7 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
+def print_student_list(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
@@ -32,33 +30,47 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def print_menu
+    #1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+def show_students
+    #2. read the input and save it into a variable
+    print_header
+    print_student_list(@students)
+    print_footer(@students)
+end
+
+def process(selection)
+    #3. do what the user has asked
+    case selection
+        when "1"
+            # input the students
+            input_students
+        when "2"
+            # show the studnets
+            show_students
+            
+        when "9"
+            exit # this will cause the program to terminate
+        else
+            puts "I don't know what you mean, try again"
+    end
+end
+
+
+
 def interactive_menu
-    students = []
     loop do
         #1. print the menu and ask the user what to do
-        puts "1. Input the students"
-        puts "2. Show the students"
-        puts "9. Exit"
-        
+        print_menu
         #2. read the input and save it into a variable
-        selection = gets.chomp
-        
         #3. do what the user has asked
-        case selection
-            when "1"
-                # input the students
-                students = input_students
-            when "2"
-                # show the studnets
-                print_header
-                print(students)
-                print_footer(students)
-                
-            when "9"
-                exit # this will cause the program to terminate
-            else
-                puts "I don't know what you mean, try again"
-        end
+        process(gets.chomp)
+        
     end
 end
 
